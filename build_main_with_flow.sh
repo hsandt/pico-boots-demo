ba323522b72e5295c93de563419fb009375bf1bb
hsandt <hs@fuwar.in>
2019-09-02 22:12:43 +0100

    [BUILD] Fixed build_main_with_flow.sh arguments and added Game: build and run - itests command

--- build_main_with_flow.sh
+++ build_main_with_flow.sh
@@ -10,12 +10,13 @@ picoboots_scripts_path="$(dirname "$0")/pico-boots/scripts"
 
 # Configuration: cartridge
 author="hsandt"
-title="pico-boots demo"
-cartridge_name="picoboots_demo"
+title="pico-boots demo with flow"
+cartridge_name="picoboots_demo_with_flow"
 
 # Build
 "$picoboots_scripts_path/build_game.sh"                               \
-  "$game_src_path/main_with_flow.lua"                                 \
+  "$game_src_path" main_with_flow.lua                                 \
   -d "$data_path/data.p8" -m "$data_path/metadata.p8"                 \
   -a "$author" -t "$title"                                            \
-  -o "$build_output_path/$cartridge_name.p8"
+  -o "$build_output_path/$cartridge_name.p8"                          \
+  --minify
