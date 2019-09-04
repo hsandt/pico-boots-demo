86d1aed470f3047720f94e8dd783c4ae3a7175c4
hsandt <hs@fuwar.in>
2019-09-04 20:40:34 +0100

    [MAIN] Fixed mouse devkit not enabled in main_with_flow

--- src/main_with_flow.lua
+++ src/main_with_flow.lua
@@ -7,10 +7,12 @@ local logging = require("engine/debug/logging")
 
 local flow = require("engine/application/flow")
 local input = require("engine/input/input")
+local ui = require("engine/ui/ui")
 
 local main_menu = require("menu/main_menu")
 local debug_demo = require("demos/debug_demo")
 local input_demo = require("demos/input_demo")
+local visual = require("resources/visual")
 
 function _init()
   -- register console log stream to output logs to the console
@@ -20,6 +22,10 @@ function _init()
   flow:add_gamestate(debug_demo())
   flow:add_gamestate(input_demo())
   flow:query_gamestate_type(main_menu.type)
+
+  -- enable mouse devkit
+  input:toggle_mouse(true)
+  ui:set_cursor_sprite_data(visual.sprites.cursor)
 end
 
 function _update60()
@@ -29,4 +35,5 @@ end
 
 function _draw()
   flow:render()
+  ui:render_mouse()
 end
