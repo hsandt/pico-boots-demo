f50f41f2c90025b902a4d8d69c3b9fba70b49592
hsandt <hs@fuwar.in>
2019-09-22 17:05:29 +0100

    [DEBUG DEMO] Updated utest to pass (although no 100% due to untested button/toggle callbacks)

--- src/demos/debug_demo_utest.lua
+++ src/demos/debug_demo_utest.lua
@@ -1,6 +1,10 @@
 require("engine/test/bustedhelper")
+local profiler = require("engine/debug/profiler")
 local debug_demo = require("demos/debug_demo")
+local wtk = require("wtk/pico8wtk")
 
+local flow = require("engine/application/flow")
+local input = require("engine/input/input")
 require("engine/render/color")
 local ui = require("engine/ui/ui")
 
@@ -10,6 +14,96 @@ describe('debug_demo', function ()
 
   before_each(function ()
     debug_demo_state = debug_demo()
+  end)
+
+  describe('init', function ()
+
+    it('should create a gui root', function ()
+      assert.are_equal(wtk.gui_root, getmetatable(debug_demo_state.gui))
+    end)
+
+    it('should add a vertical layout with 10 elements to the gui root', function ()
+      assert.are_equal(wtk.vertical_layout, getmetatable(debug_demo_state.v_layout))
+      assert.are_equal(debug_demo_state.v_layout, debug_demo_state.gui.children[1])
+      assert.are_equal(10, #debug_demo_state.v_layout.children)
+      -- we don't detail the elements further, as it would mostly be testing data
+    end)
+
+  end)
+
+  describe('update', function ()
+
+    setup(function ()
+      stub(wtk.gui_root, "update")
+      stub(debug_demo, "_go_back")
+    end)
+
+    teardown(function ()
+      wtk.gui_root.update:revert()
+      debug_demo._go_back:revert()
+    end)
+
+    after_each(function ()
+      wtk.gui_root.update:clear()
+      debug_demo._go_back:clear()
+    end)
+
+    it('should update debug_demo gui', function ()
+      debug_demo_state:update()
+
+      local s = assert.spy(debug_demo_state.gui.update)
+      s.was_called(1)
+      s.was_called_with(match.ref(debug_demo_state.gui))
+    end)
+
+    it('(when no input is down) it should not call _go_back', function ()
+      debug_demo_state:update()
+
+      local s = assert.spy(debug_demo_state._go_back)
+      s.was_not_called()
+    end)
+
+    describe('(when x just pressed)', function ()
+
+      setup(function ()
+        input.players_btn_states[0][button_ids.left] = btn_states.pressed
+        input.players_btn_states[0][button_ids.x] = btn_states.just_pressed
+      end)
+
+      teardown(function ()
+        input:init()
+      end)
+
+      it('it should call _go_back', function ()
+        debug_demo_state:update()
+
+        local s = assert.spy(debug_demo_state._go_back)
+        s.was_called(1)
+        s.was_called_with(match.ref(debug_demo_state))
+      end)
+
+    end)
+
+  end)
+
+  describe('_go_back', function ()
+
+    setup(function ()
+      stub(flow, "query_gamestate_type")
+    end)
+
+    teardown(function ()
+      flow.query_gamestate_type:revert()
+    end)
+
+    it('should enter the main_menu state', function ()
+      debug_demo_state:_go_back()
+
+      local s = assert.spy(flow.query_gamestate_type)
+      s.was_called(1)
+      s.was_called_with(flow, ':main_menu')
+    end)
+
   end)
 
   describe('render', function ()
@@ -40,8 +134,9 @@ describe('debug_demo', function ()
       debug_demo_state:render()
 
       local s = assert.spy(ui.print_centered)
-      s.was_called(1)
+      s.was_called(2)
       s.was_called_with("debug demo", 64, 6, colors.white)
+      s.was_called_with("(x: back to main menu)", 64, 12, colors.white)
     end)
 
   end)
