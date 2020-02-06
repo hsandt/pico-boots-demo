1baa1b88c1facbef7e3edc8c07fa7ca1f4ef1393
Hyper Sonic <hs@fuwar.in>
2020-02-06 18:13:39 +0000

    [BUILD] Updated build_main_with_flow to match build_game

--- build_main_with_flow.sh
+++ build_main_with_flow.sh
@@ -1,22 +1,84 @@
 #!/bin/bash
 
-# Build game with picotool
+# This is essentially a proxy script for pico-boots/scripts/build_cartridge.sh.
+# However, this is also where you define game information and defined symbols per config.
 
 # Configuration: paths
+picoboots_scripts_path="$(dirname "$0")/pico-boots/scripts"
 game_src_path="$(dirname "$0")/src"
 data_path="$(dirname "$0")/data"
 build_output_path="$(dirname "$0")/build"
-picoboots_scripts_path="$(dirname "$0")/pico-boots/scripts"
 
 # Configuration: cartridge
 author="hsandt"
 title="pico-boots demo with flow"
 cartridge_name="picoboots_demo_with_flow"
 
-# Build
-"$picoboots_scripts_path/build_game.sh"                               \
-  "$game_src_path" main_with_flow.lua                                 \
-  -d "$data_path/data.p8" -m "$data_path/metadata.p8"                 \
-  -a "$author" -t "$title"                                            \
-  -o "$build_output_path/$cartridge_name.p8"                          \
-  --minify
+help() {
+  echo "Build a PICO-8 cartridge with the passed config."
+  usage
+}
+
+usage() {
+  echo "Usage: test.sh [CONFIG]
+
+ARGUMENTS
+  CONFIG                    Build config. Determines defined preprocess symbols.
+                            (default: 'debug')
+
+  -h, --help                Show this help message
+"
+}
+
+# Default parameters
+config='debug'
+
+# Read arguments
+# https://stackoverflow.com/questions/192249/how-do-i-parse-command-line-arguments-in-bash
+roots=()
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    -h | --help )
+      help
+      exit 0
+      ;;
+    -* )    # unknown option
+      echo "Unknown option: '$1'"
+      usage
+      exit 1
+      ;;
+    * )     # store positional argument for later
+      positional_args+=("$1")
+      shift # past argument
+      ;;
+  esac
+done
+
+if ! [[ ${#positional_args[@]} -ge 0 && ${#positional_args[@]} -le 1 ]]; then
+  echo "Wrong number of positional arguments: found ${#positional_args[@]}, expected 1."
+  echo "Passed positional arguments: ${positional_args[@]}"
+  usage
+  exit 1
+fi
+
+if [[ ${#positional_args[@]} -ge 1 ]]; then
+  config="${positional_args[0]}"
+fi
+
+# Define symbols from config
+symbols=''
+
+if [[ $config == 'debug' ]]; then
+  symbols='assert,log,visual_logger,tuner,profiler,mouse'
+fi
+
+# Build from main
+"$picoboots_scripts_path/build_cartridge.sh"             \
+  "$game_src_path" main_with_flow.lua                              \
+  -d "$data_path/data.p8" -M "$data_path/metadata.p8"    \
+  -a "$author" -t "$title"                               \
+  -p "$build_output_path"                                \
+  -o "$cartridge_name"                                   \
+  -c "$config"                                           \
+  -s "$symbols"                                          \
+  --minify-level 2
